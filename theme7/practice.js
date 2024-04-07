335edbbc598144faaeb01ac3c4b2901fd4f5e75f
litak <aazizaliev2403@gmail.com@mail.ru>
2024-04-07 21:45:27 +0500

    update

--- theme7/practice.js
+++ theme7/practice.js
@@ -1,1 +1,45 @@
+// let answer = prompt("Введите Сообщение")
+// let result = [answer.split(" ")]
+// console.log(result)
 
+//  - - - - - - - - - - - - - - - - - - - - - - -  
+// 1 - задание
+
+// let x = [1,2,3,4]
+// console.log(x[1])
+
+//  - - - - - - - - - - - - - - - - - - - - - - -  
+// 2 - задание
+
+// let z = [1, 2, 3, 4, 5]
+// let q = z[z.length-1]
+// if (q % 2 == 0) {console.log("чётное")}
+// else { console.log("не чётное")}
+
+//  - - - - - - - - - - - - - - - - - - - - - - -  
+// 3 - задание
+
+// let answer = prompt("Введите сообщение") // 20 с // 20 10 5 
+// let w1 = parseInt(answer / 2)
+// let w2 = parseInt(answer / 4)
+// let res = [w1,w2]
+// console.log(res)
+
+//  - - - - - - - - - - - - - - - - - - - - - - -  
+// 4 - задание
+
+// let n = prompt("сообщение")
+// let m = ["abc","afc", "ss","str"]
+// let middle = parseInt(m.length / 2)
+// let l = m.splice(middle,0,n)
+// console.log(m)
+
+//  - - - - - - - - - - - - - - - - - - - - - - -  
+// 5 - задание 
+
+// let a = [1,2,3]
+// let t = a.pop()
+// if (t % 2 == 0) {console.log("чётное")}
+// else { console.log("не чётное")}
+
+//  - - - - - - - - - - - - - - - - - - - - - - -
