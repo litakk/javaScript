4c01d0e1cd1c7dda995dbb7b868284177f1b522e
Feruz <140828663+litakk@users.noreply.github.com>
2024-04-16 15:33:41 +0500

    upd

---
+++ theme10/theme10.js
@@ -0,0 +1,139 @@
+// let arr = ['a','b','c']
+
+// ТОЛЬКО ЧАСТЬ
+// let result = arr.filter(value => { // ПРОПУСКАЕТ ТОЛЬКО ТОТ КОТОРЫЙ ПРОХОДИТ ПРОВЕРКУ
+//     if (value == 'a') {
+//         result.push(value)
+//     } else if (value == 'e') {
+//         result.push(value)
+//     } else if (value == 'g') {
+//         result.push(value)
+//     }
+// })
+// console.log(result)
+
+// ТОЛЬКО 1
+// let result = arr.find(value => { // он находит только 1 и остановится
+//     if (value == 'a') {
+//         result.push(value)
+//     } else if (value == 'e') {
+//         result.push(value)
+//     } else if (value == 'g') {
+//         result.push(value)
+//     }
+// })
+// console.log(result)
+
+// ВСЁ БЕРЕТ
+// MAP => Перебирает массив и возвращает 
+
+// let result = arr.map(value => { // ВОЗМЁТ ВСЁ ТО ЧТО НАХОДИТСЯ В МАССИВЕ
+//     result.push(value)
+// })
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+// REDUCE - берёт всё но из всех создаёт 1 значение
+// Аккумулятор - это то место где 
+// [].reduce((acc, next) => {})
+
+
+// let arr = [1, 2, 3, 4, 5]
+// let result = arr.reduce((acc, next) => {
+//    return acc * next
+// })
+//  acc=1, next=2  =>   1*2   =>   2 
+//  acc=2, next=3  =>   2*3   =>   6
+//  acc=6, next=4  =>   6*4   =>   24
+//  acc=24, next=5  =>  24*5  =>   120
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// words = ["apple", 'banana', 'cherry', 'blueberry', 'strawbarry']
+
+// let longest = words.reduce((acc, next) => {
+//     if (acc.length > next.length) {
+//         return acc
+//     } else {
+//         return next
+//     }
+// })
+
+// console.log(longest)
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+let words = ['apple', 'banana','charry', 'bluebarry','strawbarry']
+
+let result = words.reduce((acc, next) => {
+    return acc + next.length
+}, 5)
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+// SETS => ХРАНИТ ТОЛЬКО УНИКАЛЬНЫЕ ЗНАЧЕНИЯ И УДАЛЯЕТ ДУБЛИКАТЫ
+
+// new Array()
+// new String()
+
+let arr = [1, 2, 3, 4, 5, 1, 5]
+
+let set = new Set(arr)
+console.log(set) // Set { 1, 2, 3 }
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// ADD TO SET => ДОБАВЛЯЕТ - НУЖНО ИМЕТЬ ВВИДУ ЧТО ОН НЕ ДОБАВЛЯЕТ ЕСЛИ ТАМ УЖЕ ЕСТЬ ЭТО ЗНАЧЕНИЕ
+
+set.add(6)
+set.add(4)
+console.log(set)
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// REMOVE FROM SET
+set.delete(3)
+console.log('DELETED:', set)
+
+set.delete(5)
+console.log('DEL NOT EXISTING ITEM:', set)
+
+
+
+
+
+
+
+
+// words = [1,2,3,4,5]
+
+// let longest = words.reduce((acc, next) => {
+//     if (acc > next) {
+//         return acc
+//     } else {
+//         return next
+//     }
+// })
+
+// console.log('большое',longest)
+
+
+// words = [1,2,3,4,5]
+
+// let longest2 = words.reduce((acc, next) => {
+//     if (acc < next) {
+//         return acc
+//     } else {
+//         return next
+//     }
+// })
+
+// console.log('маленькое', longest2)
+
+
+
+
+
+
+
