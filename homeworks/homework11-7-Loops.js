db952fe40319717b52eb0ac3255bbe7a79a8ce1e
Feruz <140828663+litakk@users.noreply.github.com>
2024-05-03 23:14:30 +0500

    rr

---
+++ homeworks/homework11-7-Loops.js
@@ -0,0 +1,57 @@
+// 7 - Loops
+
+// BEGINNER LEVEL (Newbie)
+
+// 1. Создайте функцию, которая принимает
+//    список целых чисел и находит сумму
+//    всех четных чисел.
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let even = []
+let odd = []
+for (let value in numbers) {
+    if (numbers[value] % 2 == 0) {
+        even.push(numbers[value])
+    } else {
+        odd.push(numbers[value])
+    }
+}
+console.log(`четные ${even} и нечётные ${odd}`)
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+// 2. Создайте функцию, которая принимает список
+//    целых чисел и находит самое большое число
+
+function maxNum(array) {
+
+    let max = array[0]
+
+    for (let num = 1; num < array.length; num++) {
+        if (array[num] > max) {
+            max = array[num]
+        }
+    }
+    return max
+}
+let array = [12, 13, 14, 15]
+console.log(maxNum(array))
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+// 2. Создайте функцию, которая принимает список
+//    целых чисел и находит самое маленькое число
+
+function minNum(array) {
+
+    let max = array[0]
+
+    for (let num = 1; num > array.length; num++) {
+        if (array[num] > max) {
+            max = array[num]
+        }
+    }
+    return max
+}
+console.log(minNum([12, 13, 14, 15]))
